4d3f0cf3ddeb553721d8efe8fafe899c25ef57f5
syleow <1802911@sit.singaporetech.edu.sg>
2020-01-13 13:11:43 +0800

    Test 2

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.helloworld
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// Testing
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
